fa42243090bbac372bbcd49bcfb5856bee457cc8
mathpvx <7547@holbertonstudents.com>
2024-04-16 16:02:26 +0200

    create endpoint that retrieves the number of each obect by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """A index file"""
 
+
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -9,3 +17,14 @@ from flask import jsonify
 def status():
     """Return status of the"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """retrieves the number of each object by type"""
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+               "reviews": Review, "states": State, "users": User}
+    obj_count = {}
+    for name, cls in classes.items():
+        obj_count[name] = storage.count(cls)
+    return jsonify(obj_count)
