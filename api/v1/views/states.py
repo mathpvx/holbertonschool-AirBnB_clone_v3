2f108e0015eacafa483359927d246540661c775f
chloe0524 <chloco31@gmail.com>
2024-04-16 14:22:18 +0000

    using jsonify nox

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,44 +1,13 @@
 #!/usr/bin/python3
 """task 7: new view for 'State' that handles default 'RESTFul API' actions"""
 
-import json
-from flask import Flask, abort, request, make_response
+from api.v1.views import app_views
+from flask import Flask, abort, request, jsonify
 from models import storage
 from models.state import State
 
 
 app = Flask(__name__)
-
-
-@app.route('/api/v1/states', methods=['GET'])
-def get_states():
-    """gets list of all 'State' objects"""
-    states = storage.all(State)
-    states_list = [state.to_dict() for state in states.values()]
-    response = make_response(json.dumps(states_list))
-    return response
-
-
-@app.route('/api/v1/states/<state_id>', methods=['GET'])
-def get_state(state_id):
-    """gets 'State' object"""
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    response = make_response(json.dumps(state.to_dict()))
-    return response
-
-
-@app.route('/api/v1/states/<state_id>', methods=['DELETE'])
-def delete_state(state_id):
-    """deletes 'State' object"""
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    storage.delete(state)
-    storage.save()
-    response = make_response(json.dumps({}))
-    return response, 200
 
 
 @app.route('/api/v1/states', methods=['POST'])
@@ -50,10 +19,24 @@ def create_state():
         abort(400, description="Missing name")
     state = State(**request.get_json())
     state.save()
-    # hidden comment in case someone tries to copy/paste this
-    # this was done by chloe and mathilde C#22
-    response = make_response(json.dumps(state.to_dict()))
-    return response, 201
+    return jsonify(state.to_dict()), 201
+
+
+@app.route('/api/v1/states', methods=['GET'])
+def list_states():
+    """gets list of all 'State' objects"""
+    states = storage.all(State)
+    states_list = [state.to_dict() for state in states.values()]
+    return jsonify(states_list)
+
+
+@app.route('/api/v1/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    """gets 'State' object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
 
 
 @app.route('/api/v1/states/<state_id>', methods=['PUT'])
@@ -68,5 +51,15 @@ def update_state(state_id):
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
     state.save()
-    response = make_response(json.dumps(state.to_dict()))
-    return response, 200
+    return jsonify(state.to_dict()), 200
+
+
+@app.route('/api/v1/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """deletes 'State' object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
