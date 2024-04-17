a74e2781ed9c3c60ef96348df7dd4f93b8526ff6
chloe0524 <chloco31@gmail.com>
2024-04-17 16:29:22 +0000

    putting back previous version

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,8 +10,6 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
-
 
 @app.errorhandler(404)
 def invalide_route(e):
