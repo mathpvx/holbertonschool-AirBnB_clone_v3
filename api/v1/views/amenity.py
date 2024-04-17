b957254f159f4096e0ec0935e290395f307ab500
chloe0524 <chloco31@gmail.com>
2024-04-17 09:10:14 +0000

    fixed import

--- api/v1/views/amenity.py
+++ api/v1/views/amenity.py
@@ -3,8 +3,9 @@
 
 from flask import abort, request, jsonify
 from models import storage
-from models.state import State
-from models.city import City
+from models.amenity import Amenity
+from api.v1.views import app_views
+
 
 
 @app.route('/api/v1/amenities', methods=['GET'])
