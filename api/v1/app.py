09d95985a613958b39057d7cefb8790aa27f8cc5
chloe0524 <127857895+chloe0524@users.noreply.github.com>
2024-04-17 18:26:27 +0200

    Update app.py added v1 in cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
